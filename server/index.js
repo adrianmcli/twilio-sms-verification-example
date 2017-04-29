120414dd5d6fd91b33b30cac0c68d83eaf707c4d
Adrian Li <li.adrianmc@gmail.com>
2017-04-28 18:08:49 -0700

    add temp code for next step of twilio integration

--- server/index.js
+++ server/index.js
@@ -12,19 +12,37 @@ app.get("/", function(req, res) {
   res.send("Hello World!");
 });
 
+// Initialize Authy stuff
+// const Client = require('authy-client').Client;
+// const authy = new Client({key: AUTHY_API_KEY});
+// const enums = require('authy-client').enums;
+
 app.post("/submit/phone", function(req, res) {
   const { phoneNumber, countryCode } = req.body;
   console.log("Phone number received: " + phoneNumber);
   console.log("Country: " + countryCode);
+
   res.status(200).json({ phoneNumber, countryCode });
+
+  // authy.startPhoneVerification({ countryCode, locale: 'en', phone: phoneNumber, via: enums.verificationVia.SMS }, function(err, res) {
+  //   if (err) throw err;
+  //   console.log('Phone information', response);
+  // });
 });
+
 
 app.post("/submit/verify", function(req, res) {
   const { code, phoneNumber, countryCode } = req.body;
   console.log("Verification code received: " + code);
   console.log("Phone number received: " + phoneNumber);
   console.log("Country: " + countryCode);
+
   res.status(200).json({ code, phoneNumber, countryCode });
+
+  // client.verifyPhone({ countryCode, phone: phoneNumber, token: code }, function(err, res) {
+  //   if (err) throw err;
+  //   console.log('Verification code is correct');
+  // });
 });
 
 app.listen(4000, function() {
